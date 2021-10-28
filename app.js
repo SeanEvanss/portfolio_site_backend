3d8487c22764caf59961da82c36d42bad23c8c4c
Sean Tang <1996sean@gmail.com>
2021-10-29 00:59:57 +0800

    Added delete functionality and began migration to mongoose package

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const PORT = 8000;
 
 const Express = require('express');
-//we might want to delete and prune body-parser
-const BodyParser = require('body-parser');
+const Mongoose = require('mongoose');
+const mongodb= require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 
@@ -13,18 +13,22 @@ const databaseName = "Messages";
 var app = Express();
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
-app.listen(PORT, () => {
+app.listen(PORT, () => {    
     MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
         if (err) {            
             console.log(err);
             throw err;            
         }
+    
         database= client.db(databaseName);
         collection= database.collection('personnel');
 
-        console.log('Connected to '+ databaseName +' database');                
+        console.log('Connected to '+ databaseName +' database on port '+PORT);               
     });
 });
+app.get('/', (req, res) => {    
+    res.send('Hello World!');
+}); 
 
 app.post('/personnel', (req, res) => {
     collection.insertOne(req.body, (err, result) => {
@@ -34,5 +38,27 @@ app.post('/personnel', (req, res) => {
             return;
         }
         res.sendStatus(201);
+    });
+});
+
+app.get('/personnel', (req, res) => {
+    collection.find({}).toArray((err, results) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        res.send(results);
+    })
+});   
+
+app.delete('/personnel', (req, res) => {
+    collection.deleteOne({"_id": new mongodb.ObjectId(req.body._id)}, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+            return;
+        }
+        res.send(result);
     });
 });
